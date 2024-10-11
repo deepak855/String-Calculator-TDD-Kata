28a050ef7860fa1945f06197e9fc640defc11cd8
nagaraju <51296278+deepak855@users.noreply.github.com>
2024-10-11 20:35:40 +0530

    Allow the add method to handle any amount of numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,13 +1,8 @@
-import re
 def add(numbers):
     if numbers == "":
-        return 0    
+        return 0   
    
-    numbers = numbers.replace('\\n', '\n') 
-    delimiter = ","
-    numbers = numbers.replace('\n', delimiter)    
-    replaced_string = re.sub(r'[^\w]', ',', numbers)       
-    number_list = [value for value in replaced_string.split(',') if value]    
+    number_list = numbers.split(',')  
     total = 0
     negatives = []    
     for num in number_list:
